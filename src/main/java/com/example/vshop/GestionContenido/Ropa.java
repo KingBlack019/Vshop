c468e637de15ae054cb1978e795d581c4d1e667f
KingBlack <vicente.marin013@gmail.com>
2024-03-14 17:01:35 +0100

    0.1.3.5

---
+++ src/main/java/com/example/vshop/GestionContenido/Ropa.java
@@ -0,0 +1,60 @@
+package com.example.vshop.GestionContenido;
+
+public class Ropa {
+    private String idRopa;
+    private String nombreRopa;
+    private String textura;
+    private String idTalla;
+    private String idEstilo;
+
+    public Ropa(String idRopa, String nombreRopa, String textura, String idTalla, String idEstilo) {
+        this.idRopa = idRopa;
+        this.nombreRopa = nombreRopa;
+        this.textura = textura;
+        this.idTalla = idTalla;
+        this.idEstilo = idEstilo;
+    }
+
+    public Ropa() {
+    }
+
+    public String getIdRopa() {
+        return idRopa;
+    }
+
+    public void setIdRopa(String idRopa) {
+        this.idRopa = idRopa;
+    }
+
+    public String getNombreRopa() {
+        return nombreRopa;
+    }
+
+    public void setNombreRopa(String nombreRopa) {
+        this.nombreRopa = nombreRopa;
+    }
+
+    public String getTextura() {
+        return textura;
+    }
+
+    public void setTextura(String textura) {
+        this.textura = textura;
+    }
+
+    public String getIdTalla() {
+        return idTalla;
+    }
+
+    public void setIdTalla(String idTalla) {
+        this.idTalla = idTalla;
+    }
+
+    public String getIdEstilo() {
+        return idEstilo;
+    }
+
+    public void setIdEstilo(String idEstilo) {
+        this.idEstilo = idEstilo;
+    }
+}
--- src/main/java/com/example/vshop/Servidor/BaseDeDatos.java
+++ src/main/java/com/example/vshop/Servidor/BaseDeDatos.java
@@ -1,11 +1,22 @@
 package com.example.vshop.Servidor;
 
+import com.example.vshop.GestionContenido.Idioma;
+import com.example.vshop.GestionContenido.Ropa;
+import com.example.vshop.GestionContenido.Tienda;
+
 import java.sql.*;
+import java.util.ArrayList;
 
 public class BaseDeDatos {
+
+    // Declaracion de variables de la database
     private static final String nameDatabase ="Vshop";
     private static final String userDatabase ="Vshop";
     private static final String passwordDatabase ="Vshop";
+
+    // Declaracion de variables de clases
+    Ropa gestionRopa = new Ropa();
+    Tienda gestionTienda = new Tienda();
 
     public boolean isConnected(){
         Connection connection = null;
@@ -38,4 +49,67 @@ public class BaseDeDatos {
             return false;
         }
     }
+
+    public boolean filtrarEstiloTienda(String estiloTienda) {
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+nameDatabase, userDatabase, passwordDatabase);
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM Usuarios WHERE nombreUsuario  = '" + username + "' AND contrasenaUsuario = '" + password +"'"
+            );
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                System.out.println("Usuario conectado correctamente");
+                return true;
+            }else{
+                System.out.println("Usuario no existe");
+                return false;
+            }
+        }catch (Exception e){
+            System.out.println("e.getMessage() = " + e.getMessage());
+            return false;
+        }
+    }
+
+    public ArrayList<String> obtenerIdiomasUsuario (String nombreUsuario){
+        ArrayList<String> idiomasHablado = new ArrayList<>();
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+nameDatabase, userDatabase, passwordDatabase);
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT idiomas.nombreIdioma FROM usuarios INNER JOIN conocimientosUsuario ON usuarios."
+            );
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()){
+                String idioma = resultSet.getString("nombreIdioma");
+                idiomasHablado.add(idioma); // se añade un idioma por cada registro de idiomas hablado por el usuario
+            }
+
+        }catch (Exception e ){
+            System.out.println("e.getMessage() = " + e.getMessage());
+        }
+        return idiomasHablado;
+    }
+
+    public ArrayList<String> obtenerLenguajeProgramacionUsuario (String nombreUsuario){
+        ArrayList<String> lenguajeProgramacionConocidos = new ArrayList<>();
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+nameDatabase, userDatabase, passwordDatabase);
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT lenguajeProgramacion.nombreLenguajeProgramacion FROM usuarios INNER JOIN conocimientosUsuario ON usuarios."
+            );
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                String lenguaje = resultSet.getString("nombreLenguajeProgramacion");
+                lenguajeProgramacionConocidos.add(lenguaje); // se añade un idioma por cada registro de idiomas hablado por el usuario
+            }
+
+        }catch (Exception e ){
+            System.out.println("e.getMessage() = " + e.getMessage());
+        }
+        return lenguajeProgramacionConocidos;
+    }
+
 }
